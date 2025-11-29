db6cd545be42b6cdde7079b02abd68d18961b1a5
Toha Hossain <tohahpro@gmail.com>
2025-11-29 11:07:31 +0600

    prisma and postgreSQL all setup complete

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import notFound from './app/middlewares/notFound';
+// import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import config from './app/config';
+
+const app: Application = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+//parser
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// app.use("/api/v1", router)
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        message: "Server is running..",
+        environment: config.node_env,
+        uptime: process.uptime().toFixed(2) + " sec",
+        timeStamp: new Date().toISOString()
+    })
+});
+
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
---
+++ src/app/config/index.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export default {
+    node_env: process.env.NODE_ENV,
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL,
+    salt_round: process.env.BCRYPT_SALT,
+    cloudinary: {
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET
+    },
+    JWT:{
+        JWT_ACCESS_SECRET : process.env.JWT_ACCESS_SECRET,
+        JWT_ACCESS_EXPIRES : process.env.JWT_ACCESS_EXPIRES,
+        
+        JWT_REFRESH_SECRET : process.env.JWT_REFRESH_SECRET,
+        JWT_REFRESH_EXPIRES : process.env.JWT_REFRESH_EXPIRES,
+        
+        RESET_PASS_SECRET : process.env.RESET_PASS_SECRET,
+        RESET_PASS_TOKEN_EXPIRES : process.env.RESET_PASS_TOKEN_EXPIRES_IN,
+        RESET_PASS_LINK : process.env.RESET_PASS_LINK,
+    },
+    emailSender: {
+        email: process.env.EMAIL,
+        app_pass: process.env.APP_PASS
+    },
+}
